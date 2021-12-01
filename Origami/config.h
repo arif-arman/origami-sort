ad1a62ef81e8dcaa95c468abdf3d6a47150fd9f1
arif-arman <arif.arman046@gmail.com>
2021-11-30 23:50:55 -0600

    updated license

--- Origami/config.h
+++ Origami/config.h
@@ -1,6 +1,23 @@
+/*--------------------------------------------------------------------------------------------
+ - Origami: A High-Performance Mergesort Framework											 -
+ - Copyright(C) 2021 Arif Arman, Dmitri Loguinov											 -
+ - Produced via research carried out by the Texas A&M Internet Research Lab                  -
+ -                                                                                           -
+ - This program is free software : you can redistribute it and/or modify                     -
+ - it under the terms of the GNU General Public License as published by                      -
+ - the Free Software Foundation, either version 3 of the License, or                         -
+ - (at your option) any later version.                                                       -
+ -                                                                                           -
+ - This program is distributed in the hope that it will be useful,                           -
+ - but WITHOUT ANY WARRANTY; without even the implied warranty of                            -
+ - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the                               -
+ - GNU General Public License for more details.                                              -
+ -                                                                                           -
+ - You should have received a copy of the GNU General Public License                         -
+ - along with this program. If not, see < http://www.gnu.org/licenses/>.                     -
+ --------------------------------------------------------------------------------------------*/
+
 #pragma once
-
-
 #define L2_BYTES MB(1)
 
 #define REG_TYPE 2		// 0: scalar, 1,2,3: sse, avx2, avx512
--- Origami/sorter.h
+++ Origami/sorter.h
@@ -1,10 +1,29 @@
+/*--------------------------------------------------------------------------------------------
+ - Origami: A High-Performance Mergesort Framework											 -
+ - Copyright(C) 2021 Arif Arman, Dmitri Loguinov											 -
+ - Produced via research carried out by the Texas A&M Internet Research Lab                  -
+ -                                                                                           -
+ - This program is free software : you can redistribute it and/or modify                     -
+ - it under the terms of the GNU General Public License as published by                      -
+ - the Free Software Foundation, either version 3 of the License, or                         -
+ - (at your option) any later version.                                                       -
+ -                                                                                           -
+ - This program is distributed in the hope that it will be useful,                           -
+ - but WITHOUT ANY WARRANTY; without even the implied warranty of                            -
+ - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the                               -
+ - GNU General Public License for more details.                                              -
+ -                                                                                           -
+ - You should have received a copy of the GNU General Public License                         -
+ - along with this program. If not, see < http://www.gnu.org/licenses/>.                     -
+ --------------------------------------------------------------------------------------------*/
+
 #pragma once
 
 #include "commons.h"
 #include "merge_utils.h"
 #include "merge_tree.h"
 #include "merger.h"
-#include "Partition.h"
+#include "partition.h"
 #include <fstream>
 
 
